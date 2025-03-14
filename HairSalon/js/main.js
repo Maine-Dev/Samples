b77bb591c65b1c407f9d1f68c8670b36cf02f5e3
Jermaine Gadsden <56989674+Maine-Dev@users.noreply.github.com>
2025-03-13 20:56:22 -0400

    Add files via upload

---
+++ HairSalon/js/main.js
@@ -0,0 +1,35 @@
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropdownMenu = document.querySelector('.dropdown__menu');
+let scrollPosition = 0;
+
+function disableScroll() {
+  scrollPosition = window.pageYOffset;
+  document.body.style.overflow = 'hidden';
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollPosition}px`;
+  document.body.style.width = '100%';
+}
+
+function enableScroll() {
+  document.body.style.overflow = '';
+  document.body.style.position = '';
+  document.body.style.top = '';
+  document.body.style.width = '';
+  window.scrollTo(0, scrollPosition);
+}
+
+toggleBtn.onclick = function() {
+  dropdownMenu.classList.toggle('open');
+  const isOpen = dropdownMenu.classList.contains('open');
+
+  toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars';
+
+  if (isOpen) {
+    disableScroll();
+  } else {
+    enableScroll();
+  }
+}
